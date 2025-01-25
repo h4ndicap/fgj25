430447dc876ddb0279ba981f65cc553e0e06f237
Samuli Kinnunen <samuli.kinnunen@movya.fi>
2025-01-25 22:36:43 +0200

    Some statusadjustments

--- src/client/imagefiles.ts
+++ src/client/imagefiles.ts
@@ -29,14 +29,13 @@ export interface ImageTransformInfo {
 
 export const imageSizes = new Map<string, ImageTransformInfo>()
 
-imageSizes.set('simpukka1.png', { scale: new Vector3().setScalar(0.5), rotation: new Euler(-Math.PI / 2), offset: new Vector3(0, 0.1, 0) })
-imageSizes.set('simpukka2.png', { scale: new Vector3().setScalar(0.5), rotation: new Euler(-Math.PI / 2), offset: new Vector3(0, 0.1, 0) })
+imageSizes.set('simpukka1.png', { scale: new Vector3().setScalar(0.5), rotation: new Euler(-Math.PI / 2), offset: new Vector3(0, 0.1, 0), shadowSize: new Vector3(0.0, 0.0, 0.0) })
+imageSizes.set('simpukka2.png', { scale: new Vector3().setScalar(0.5), rotation: new Euler(-Math.PI / 2), offset: new Vector3(0, 0.1, 0), shadowSize: new Vector3(0.0, 0.0, 0.0) })
 imageSizes.set('kivikasvi.png', { scale: new Vector3().set(836 / 1411, 1, 1), offset: new Vector3(0, 0.35, 0), shadowSize: new Vector3(0.0, 0.0, 0.0) })
 imageSizes.set('laulanen.png', { scale: new Vector3().set(1, 1, 1), offset: new Vector3(0, 0.4, 0), shadowSize: new Vector3(0.3, 0.1, 0.1) })
 imageSizes.set('likainenlautanen.png', { scale: new Vector3().set(1, 1, 1), offset: new Vector3(0, 0.4, 0), shadowSize: new Vector3(0.3, 0.1, 0.1) })
-imageSizes.set('lusikka.png', {})
 imageSizes.set('lusikkalikainen.png', { scale: new Vector3().set(1, 300 / 200, 1), rotation: new Euler(-Math.PI / 2), offset: new Vector3(0, 0.0, 0), shadowSize: new Vector3(0.0, 0.0, 0.0) })
-imageSizes.set('pesuaine.png', { scale: new Vector3().set(1, 787 / 579, 1), offset: new Vector3(0, 0.75, 0), shadowSize: new Vector3(0.2, 0.2, 0.2) })
+imageSizes.set('pesuaine.png', { scale: new Vector3().set(0.5, 787 / 579 * 0.5, 0.5), offset: new Vector3(0, 0.75, 0), shadowSize: new Vector3(0.2, 0.2, 0.2) })
 // imageSizes.set('simpukka2.png', new Vector3().setScalar(0.5))
 // imageSizes.set('kivikasvi.png', new Vector3().setScalar(0.5))
 
